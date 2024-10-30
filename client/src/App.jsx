4202d628591ab0a892445217d650a87d9b1e617b
Danish Ali <dali27363@gmail.com>
2024-10-30 16:04:25 +0500

    implement routes protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy } from "react";
+import ProtectedRoute from "./components/auth/protected-route";
+import NotFound from "./pages/not-found";
 
 const Home = lazy(() => import("./pages/home"));
 const Chat = lazy(() => import("./pages/chat"));
 const Group = lazy(() => import("./pages/group"));
 const Login = lazy(() => import("./pages/login"));
 
+const user = true;
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/group" element={<Group />} />
-        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/group" element={<Group />} />
+        </Route>
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute user={!user} redirect="/">
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
