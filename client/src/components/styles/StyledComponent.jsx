ed3ef3bebc8ab4d0b596ff051b51dfc86d0efaee
Danish Ali <dali27363@gmail.com>
2024-11-01 01:04:04 +0500

    design admin dashboard

--- client/src/components/styles/StyledComponent.jsx
+++ client/src/components/styles/StyledComponent.jsx
@@ -1,29 +1,29 @@
-import { styled } from "@mui/material";
+import { Skeleton, keyframes, styled } from "@mui/material";
 import { Link as LinkComponent } from "react-router-dom";
-import { grayColor } from "../../constants/color";
+import { grayColor, matBlack } from "../../constants/color";
 
-export const VisuallyHiddenInput = styled("input")({
-    border: 0,
-    clip: "rect(0 0 0 0)",
-    height: 1,
-    margin: 1,
-    overflow: "hidden",
-    padding: 0,
-    position: "absolute",
-    whiteSpace: "nowrap",
-    width: 1,
+const VisuallyHiddenInput = styled("input")({
+  border: 0,
+  clip: "rect(0 0 0 0)",
+  height: 1,
+  margin: -1,
+  overflow: "hidden",
+  padding: 0,
+  position: "absolute",
+  whiteSpace: "nowrap",
+  width: 1,
 });
 
-export const Link = styled(LinkComponent)`
-    text-decoration: none;
-    color: black;
-    padding: 1rem;
-    &:hover {
-        background-color: rgba(0,0,0,0.3);
-    }
+const Link = styled(LinkComponent)`
+  text-decoration: none;
+  color: black;
+  padding: 1rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 `;
 
-export const InputBox = styled("input")`
+const InputBox = styled("input")`
   width: 100%;
   height: 100%;
   border: none;
@@ -32,3 +32,46 @@ export const InputBox = styled("input")`
   border-radius: 1.5rem;
   background-color: ${grayColor};
 `;
+
+const SearchField = styled("input")`
+  padding: 1rem 2rem;
+  width: 20vmax;
+  border: none;
+  outline: none;
+  border-radius: 1.5rem;
+  background-color: ${grayColor};
+  font-size: 1.1rem;
+`;
+
+const CurveButton = styled("button")`
+  border-radius: 1.5rem;
+  padding: 1rem 2rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background-color: ${matBlack};
+  color: white;
+  font-size: 1.1rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
+
+const bounceAnimation = keyframes`
+0% { transform: scale(1); }
+50% { transform: scale(1.5); }
+100% { transform: scale(1); }
+`;
+
+const BouncingSkeleton = styled(Skeleton)(() => ({
+  animation: `${bounceAnimation} 1s infinite`,
+}));
+
+export {
+  CurveButton,
+  SearchField,
+  InputBox,
+  Link,
+  VisuallyHiddenInput,
+  BouncingSkeleton,
+};
