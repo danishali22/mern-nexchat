b857b8ab54c163ecd132e394de547824cbfeea00
Danish Ali <dali27363@gmail.com>
2024-11-01 19:46:18 +0500

    create schema of all models

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 import express from "express";
+import userRoute from "./routes/user.js";
 
 const app = express();
 
 const port = 3000;
+app.use("/user", userRoute);
+
+app.get("/", (req,res)=>{
+    res.send("Home Route");
+})
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
---
+++ server/models/request.js
@@ -0,0 +1,27 @@
+import { model, models, Schema } from "mongoose";
+
+const schema = new Schema(
+  {
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "accepted", "rejected"],
+    },
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver:{
+        
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Request = models.Request || model("Request", schema);
