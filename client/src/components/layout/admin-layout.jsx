3246d96eec0200c2c28f094d08e1d27c9d2542aa
Danish Ali <dali27363@gmail.com>
2024-12-24 15:39:19 +0500

    update admin-layout.jsx, admin-login.jsx and auth.js

--- client/src/components/layout/admin-layout.jsx
+++ client/src/components/layout/admin-layout.jsx
@@ -20,6 +20,8 @@ import {
   import { useState } from "react";
   import { Link as LinkComponent, Navigate, useLocation } from "react-router-dom";
   import { grayColor, matBlack } from "../../constants/color";
+import { useDispatch, useSelector } from "react-redux";
+import { adminLogout } from "../../redux/thunks/admin";
   
   const Link = styled(LinkComponent)`
     text-decoration: none;
@@ -56,9 +58,10 @@ import {
   
   const Sidebar = ({ w="100%" }) => {
     const location = useLocation();
+    const dispatch = useDispatch();
   
     const logoutHandler = () => {
-      console.log("logout");
+      dispatch(adminLogout());
     };
   
     return (
@@ -101,7 +104,7 @@ import {
   };
   
   const AdminLayout = ({ children }) => {
-    const isAdmin = true;
+    const { isAdmin } = useSelector((state) => state.auth);
   
     const [isMobile, setIsMobile] = useState(false);
   
--- client/src/pages/admin/admin-login.jsx
+++ client/src/pages/admin/admin-login.jsx
@@ -1,19 +1,28 @@
 import { useInputValidation } from "6pp";
-import {
-  Button,
-  Container,
-  Paper,
-  TextField,
-  Typography
-} from "@mui/material";
-import { bgGradient } from "../../constants/color";
+import { Button, Container, Paper, TextField, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { bgGradient } from "../../constants/color";
+import { adminLogin, getAdmin } from "../../redux/thunks/admin";
 
-const Login = () => {
-  const isAdmin = true;
+const AdminLogin = () => {
+  const { isAdmin } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
   const secretKey = useInputValidation("");
 
-  if(isAdmin) return <Navigate to="/admin/dashboard" />
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(adminLogin(secretKey.value));
+  };
+
+  useEffect(() => {
+    dispatch(getAdmin());
+  }, [dispatch]);
+
+  if (isAdmin) return <Navigate to="/admin/dashboard" />;
 
   return (
     <div
@@ -22,8 +31,8 @@ const Login = () => {
       }}
     >
       <Container
-        maxWidth="xs"
         component={"main"}
+        maxWidth="xs"
         sx={{
           height: "100vh",
           display: "flex",
@@ -40,32 +49,41 @@ const Login = () => {
             alignItems: "center",
           }}
         >
-              <Typography variant="h4">Admin Login</Typography>
-              <form style={{ width: "100%", marginTop: "1rem" }}>
-                <TextField
-                  required
-                  fullWidth
-                  label="Secret Key"
-                  type="password"
-                  margin="normal"
-                  variant="outlined"
-                  value={secretKey.value}
-                  onChange={secretKey.changeHandler}
-                />
-                <Button
-                  variant="contained"
-                  fullWidth
-                  color="primary"
-                  type="submit"
-                  sx={{ marginTop: "1rem" }}
-                >
-                  Login
-                </Button>
-              </form>
+          <Typography variant="h5">Admin Login</Typography>
+          <form
+            style={{
+              width: "100%",
+              marginTop: "1rem",
+            }}
+            onSubmit={submitHandler}
+          >
+            <TextField
+              required
+              fullWidth
+              label="Secret Key"
+              type="password"
+              margin="normal"
+              variant="outlined"
+              value={secretKey.value}
+              onChange={secretKey.changeHandler}
+            />
+
+            <Button
+              sx={{
+                marginTop: "1rem",
+              }}
+              variant="contained"
+              color="primary"
+              type="submit"
+              fullWidth
+            >
+              Login
+            </Button>
+          </form>
         </Paper>
       </Container>
     </div>
   );
 };
 
-export default Login;
+export default AdminLogin;
